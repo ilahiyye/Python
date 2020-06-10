887b0e5a924da09ebbca644508e47511c74a234f
İlahiye Musayeva <50544941+ilahiyye@users.noreply.github.com>
2020-06-10 16:59:23 +0400

    Update fizzBuzz.py

--- Week1/fizzBuzz.py
+++ Week1/fizzBuzz.py
@@ -3,7 +3,7 @@ fizzBuzz_list = []
 for i in range(1,100): 
     if i%15==0:
        fizzBuzz_list.append("fizzBuzz")
-    if i%3 == 0: 
+    elif i%3 == 0: 
         fizzBuzz_list.append("fizz")
     elif i%5 ==0:
         fizzBuzz_list.append( "buzz")
