940bba12fc9bc927e7c05b11c8268386edba0cea
İlahiye Musayeva <50544941+ilahiyye@users.noreply.github.com>
2020-06-10 15:54:46 +0400

    Update fizzBuzz.py

--- Week1/fizzBuzz.py
+++ Week1/fizzBuzz.py
@@ -6,12 +6,12 @@ This is a temporary script file.
 """
 for i in range(1,100): 
     if i%3 == 0: 
-        print("fizz")
+        print(i, "fizz")
     if i%5 ==0:
-        print("buzz")
+        print(i, "buzz")
     if i%15==0:
-        print("fizzBuzz")
+        print(i, "fizzBuzz")
     else:
-        print("nothing")
+        print(i, "nothing")
     
 
