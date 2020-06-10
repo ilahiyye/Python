d7b822ffd24a2977cbb2b519b68d3d81bb032547
ilahiyye <ilahiyye@gmail.com>
2020-06-10 15:52:24 +0400

    first commit

---
+++ Week1/fizzBuzz.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+for i in range(1,100): 
+    if i%3 == 0: 
+        print("fizz")
+    if i%5 ==0:
+        print("buzz")
+    if i%15==0:
+        print("fizzBuzz")
+    else:
+        print("nothing")
+    
+
