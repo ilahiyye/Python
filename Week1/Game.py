d0a0d34d9b6abbd412bd73dc59bf2bc3923bbd1a
İlahiye Musayeva <50544941+ilahiyye@users.noreply.github.com>
2020-06-11 16:58:57 +0400

    Add files via upload

---
+++ Week1/Game.py
@@ -0,0 +1,28 @@
+import random
+
+randomList = ["rock", "scissors", "paper"]
+u = 0
+comp = 0
+def randomChoise():
+    return random.choise(randomList)
+
+while True:
+    user = input("input 'rock', 'scissors', 'paper' or 'exit'")
+    computer = randomChoise()
+    if ((user == 'rock' and computer == 'scissors') or (user == "scissors" and computer == "paper")):
+        u = +1
+        print("User wins!")
+    elif (user == 'rock' and computer == "paper"):
+        comp = +1
+        print("Coputer wins!")
+    elif (user == "exit"):
+        break
+
+    else:
+        print("Error: Wrong input!")
+
+    print(f'Result: User - {u} : Computer - {comp}')
+
+    
+
+
