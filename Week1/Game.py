97c141153e511b5ebe0f0ee5d0cd37b573519691
İlahiye Musayeva <50544941+ilahiyye@users.noreply.github.com>
2020-06-12 13:22:01 +0400

    Update Game.py

--- Week1/Game.py
+++ Week1/Game.py
@@ -3,28 +3,40 @@ import random
 randomList = ["rock", "scissors", "paper"]
 u = 0
 comp = 0
+tie = 0
 def randomChoise():
     return random.choice(randomList)
 
 while True:
-    user = input("input 'rock', 'scissors', 'paper' or 'exit'")
-    computer = randomChoice()
-    if ((user == 'rock' and computer == 'scissors') or (user == "scissors" and computer == "paper")):
-        u = +1
-        print("User wins!")
-    elif (user == 'rock' and computer == "paper"):
-        comp = +1
+    user = input("input 'rock', 'scissors', 'paper' or 'exit':  ")
+    computer = randomChoise()
+    if ((user == 'rock' and computer == 'scissors') or (user == "scissors" and computer == "paper") or (user == 'paper' and computer == 'rock')):
+        
+        u += 1
+
+        print(f'Your choise: {user}\nComputer choice: {computer}')
+        print("Human wins!")
+        print(f'Games Played: {u + comp + tie}, Human: {u}, Computer: {comp}, Tie: {tie}')
+    
+    elif ((user == 'rock' and computer == "paper") or (user == 'scissors' and computer == 'rock')or (user == 'paper' and computer == 'scissors')):
+        
+        comp += 1
+
+        print(f'Your choise: {user}\nComputer choice: {computer}')
         print("Coputer wins!")
+        print(f'Games Played: {u + comp + tie}, Human: {u}, Computer: {comp}, Tie: {tie}')
+    
+    elif ((user == computer)):
+        
+        tie += 1
+
+        print(f'Your choise: {user}\nComputer choice: {computer}')
+        print("Tie!")
+        print(f'Games Played: {u + comp + tie}, Human: {u}, Computer: {comp}, Tie: {tie}')
+
     elif (user == "exit"):
+        print("Your choice: Exit\nprint('Exiting program.')")
         break
 
     else:
         print("Error: Wrong input!")
-    
-    print(f'Total total number of games {u + comp}')
-    print(f'Result: User - {u} : Computer - {comp}')
-    
-
-    
-
-
