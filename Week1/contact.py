59a35b1696a969c889cde480a4bfcc1c9e3c85d6
İlahiye Musayeva <50544941+ilahiyye@users.noreply.github.com>
2020-06-17 15:43:09 +0400

    Update contact.py

--- Week1/contact.py
+++ Week1/contact.py
@@ -5,15 +5,15 @@ contact = {
     'Terlan': '05997869547',
     'Sevda' : '05953900410',
     }
-def contact_Clear():
+def contact_clear():
     return contact.clear()
 
-def contact_Add(name='None', number='None'):
+def contact_add(name='None', number='None'):
     contact[name] = f'{number},'
     return print(f"{name} add to contact")
 
     
-def contact_SearchNumber(name='None'):
+def contact_search_number(name='None'):
     return contact.setdefault(name, 'No results')
 
 def contact_delete(name='None'):
@@ -21,9 +21,7 @@ def contact_delete(name='None'):
         del contact[name] 
         return True
     
-
-
-def allContact():
+def all_contact():
     for k , v in contact.items():
         print(f'{k} : {v}\n')
 
@@ -32,19 +30,19 @@ while True:
     command = input("input command -'Clear Contact', 'Add Contact', 'Search', 'Delete Contact', 'Contact List', 'Exit' : ")
     command = command.lower()
     if (command == 'clear contact'):
-        contact_Clear()
+        contact_clear()
         print("Your contact clear!")
 
     elif (command == 'add contact'):
         name   = input('input name: ')
         number = input('input number: ')
-        contact_Add(name, number)
-        allContact()
+        contact_add(name, number)
+        all_contact()
         
 
     elif (command == "search"):
         name = input('input name: ')
-        result = contact_SearchNumber(name) 
+        result = contact_search_number(name) 
         if result != "No results":
             print(f"{name}'s number is {result}")
         else:
@@ -56,14 +54,14 @@ while True:
         
         if contact_delete(name):
             contact_delete(name)
-            allContact()
+            all_contact()
             print(f'{name} was delete in contact!')
         else:
             print("Wrong Contact Name!")
         
     
     elif (command == 'contact list'):
-        allContact()
+        all_contact()
 
     elif (command == 'exit'):
         print("Exiting program...")
